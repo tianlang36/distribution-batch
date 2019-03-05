f49ad8d832c0697b896dd8560c752dd977b4c2c4
peter <biz0petter@126.com>
2019-03-05 14:01:58 +0800

    删除目录src/main

--- src/main/java/cn/rdtimes/disb/client/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 客户端用来发布和操控任务包
- * Created by BZ on 2019/2/13.
- */
-package cn.rdtimes.disb.client;
--- src/main/java/cn/rdtimes/disb/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 简单分布式批处理
- * Created by BZ on 2019/2/12.
- */
-package cn.rdtimes.disb;
